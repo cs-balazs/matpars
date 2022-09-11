933cf4e77658243e151b1e822a7f32a3a9b6191a
Csete Balázs <csetebalu@gmail.com>
2022-09-11 16:39:55 +0200

    main cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,8 @@ fn main() {
     let mut parsed = matpars::parse("3* z - x^2 + 3*x + (8*x - 21*9) + 5 / ( 10 * a - 3 ) + x");
     exporter::MermaidExporter::export(&parsed.tree, "mermaid");
 
-    println!("{:?}", parsed.variables);
-
     parsed.set_variable("x", 10.0f64).unwrap();
-
-    println!("{:?}", parsed.variables);
-
     parsed.set_variable("x", 1.1653414f64).unwrap();
-
-    println!("{:?}", parsed.variables);
 
     println!("{}", parsed.eval().unwrap());
 
